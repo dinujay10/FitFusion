0ace578fedbc4c50398ced770f447327de4e36ad
jagath <jagathdammika17595@gmail.com>
2024-01-04 23:18:40 +0530

    modify gymmanager dashboard

--- app/controllers/Gymmanagerdash.php
+++ app/controllers/Gymmanagerdash.php
@@ -8,8 +8,24 @@ class Gymmanagerdash {
         if(!isset($_SESSION['email'])){
            redirect('login');
         }
-        // $member = new Member;
-        // $arr['name'] = "Mary";
+        $data=[];
+
+         $notifications = new Managernotifications;
+         $arr['manageremail'] = $_SESSION['email'];
+         $notificationsdata=$notifications->where($arr);
+         foreach($notificationsdata as $nt){
+            $arr2['nmsg']=$nt->nmsg;
+            $arr2['nsub']=$nt->nsub;
+            array_push($data,$arr2);
+
+         }
+
+         $notifications = array(
+            array('id' => 1, 'message' => 'New message 1'),
+            array('id' => 2, 'message' => 'New message 2'),
+            // ... add more notifications as needed
+        );
+         echo json_encode($notifications);
         // $arr['age'] = 30;
 
         // $result = $user->findAll();
