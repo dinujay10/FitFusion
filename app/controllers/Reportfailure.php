3f428696bb48fbb99cb18e60b1868a4d97ae3176
IsinduTharudinitha <tharudinitha@gmail.com>
2024-01-05 14:58:36 -0800

    changed the report failure controller

--- app/controllers/Reportfailure.php
+++ app/controllers/Reportfailure.php
@@ -28,24 +28,125 @@ class Reportfailure{
                 //     echo "UNAUTHORIZED ACCESS";
                 // }
                
-            }
-            ////////////////////////////////
+                }
 
-            if ($_SERVER['REQUEST_METHOD']=='POST') {
+            // $arr['id'] = $data['id'];
+                        ////////////////////////////////
+              $temp=$usertable->first($data);
+            //   print_r($data);
+
+              if($temp){
+
+                // print_r($temp);
+                  $data['id']=$temp->id;
+                  $data['failure']=$temp->failure;
+                  $data['notes']=$temp->notes;
+                //   print_r($data);
+
+                  $this->view('alreadyreported', $data);
+
+              }
+              else{
+                       
+                   
+          //   if ($_SERVER['REQUEST_METHOD']=='POST') {
+              
+          //     $this->view('reportfailure',$data);
+          //     print_r($_data);
+          //     $_POST['id']=$_data['id'];
+
+           
+          //     if($usertable->unique($data)  && $usertable->validate($_POST))
+          //     {
+          //          print_r($_POST);
+
+
+          //         if( !empty($_POST['failure']) && !empty($_POST['notes'])){
+          //           //  $_POST['id']=$data['id'];
+                    
+          //   $usertable->insert($_POST['id'], $_POST);
+          //   // this->view('machinefailure');
+               
+                 
+          //   //       $usertable->insert($_POST);
+          //         redirect('machinefailure');
+          //     }
+          //   }
+          // }
+
+
+
+            //     // $this->view('reportfailure',$data);
+            //     if ($_SERVER['REQUEST_METHOD']=='GET') {
+            //            $this->view('reportfailure',$data);
+            //         // $this->view('reportfailure', $data);
+
+            //         $_GET['id']=$data['id'];
+            // //        redirect('reportfailure', $data);
+            // // //    
+            //         if( !empty($_GET['failure']) && !empty($_GET['notes'])){
+            //                 //  $_POST['id']=$data['id'];
+                            
+            //         $usertable->insert($_GET['id'], $_GET);
+            //         this->view('machinefailure');
+                       
+            //         // redirect('machinefailure');
+            //     //    $data['errors'] = $usertable->errors;
+            //     }
+            //   }
+        
+          
+
+
+
+
+
+
+            // if ($_SERVER['REQUEST_METHOD']=='POST') {
+             
+                //  redirect('reportfailure');
+                $this->view('reportfailure',$data);
+
+                if( !empty($_POST['failure']) && !empty($_POST['notes'])){
+                     $_POST['id']=$data['id'];
+                     $usertable->insert($_POST);
+
+                     
+                     redirect('machinefailure');
+                 }
+                 else{
+                     $data['errors']='Please fill all the fields';
+                 }
+                 
+                //  $usertable->update($_POST['id'],$_POST);
+                //  redirect('machinefailure');
+                // $data['errors'] = $usertable->errors;
+
                 
-               if( !empty($_POST['failure']) && !empty($_POST['notes'])){
-                    $_POST['id']=$data['id'];
-                    $usertable->insert($_POST);
-                    //redirect('machinefailure');
-                }
-                else{
-                    $data['errors']='Please fill all the fields';
-                }
+                // this->view('reportfailure', $data);
+            // } 
+            // //   }
+
+            
+
+
+            // if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+
                 
-                // $usertable->update($_POST['id'],$_POST);
-                // redirect('machinefailure');
-               // $data['errors'] = $usertable->errors;
-            }
+            //    if( !empty($_POST['failure']) && !empty($_POST['notes'])){
+            //         $_POST['id']=$data['id'];
+            //         $usertable->insert($_POST);
+            //         redirect('machinefailure');
+            //     }
+            //     else{
+            //         $data['errors']='Please fill all the fields';
+            //     }
+                
+            //     // $usertable->update($_POST['id'],$_POST);
+            //     // redirect('machinefailure');
+            //    // $data['errors'] = $usertable->errors;
+            // }
             // if($_SERVER['REQUEST_METHOD']=='GET'){
             //     if(isset($_GET['deleteid'])){
             //         $idd=$_GET['deleteid'];
@@ -54,9 +155,9 @@ class Reportfailure{
             //         redirect('manageresources');
             //     }
             // }
-             $this->view('reportfailure', $data);
+            //  $this->view('reportfailure', $data);
            
-        }
+        
 
          
    
@@ -65,4 +166,6 @@ class Reportfailure{
     //     show("from the edit function");
     //     $this->view('home');
     // }
+    }
+}
 }
