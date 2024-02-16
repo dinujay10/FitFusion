25e73c871e0f7432e2ef891e08d6c6d8ebcacee9
IsinduTharudinitha <tharudinitha@gmail.com>
2024-02-15 22:43:32 -0800

    flexibility controller

--- app/controllers/Flexibility.php
+++ app/controllers/Flexibility.php
@@ -39,11 +39,11 @@ class Flexibility{
 
 
 
-                $equipmentData = $table2->findAll(['id'=>$temp->id]);
+                $equipmentData = $table2->where(['id'=>$temp->id]);
                 // Store equipment data in the cardio plan data
                 $flexibilityPlanData['equipment'] = $equipmentData;
 
-                $supplimentData = $table3->findAll(['id' => $temp->id]);
+                $supplimentData = $table3->where(['id' => $temp->id]);
                 // Store supplement data in the cardio plan data
                 $flexibilityPlanData['suppliment'] = $supplimentData;
                  
