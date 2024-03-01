37a091ea0cc29c64241af24995e003b915338cce
jagath <jagathdammika17595@gmail.com>
2024-03-01 07:30:20 +0530

    manger email sending

---
+++ public/assets/mailconfig.php
@@ -0,0 +1,14 @@
+<?php
+    define('MAILHOST',"smtp.gmail.com");
+
+    define('USERNAME',"jagathdammika17595@gmail.com");
+
+    define('PASSWORD',"lzat czas smgm jymr");
+//email addres from which email is sent
+    define('SEND_FROM',"jagathdammika17595@gmail.com");
+//name of website email is sent
+    define('SEND_FROM_NAME',"Digitalfox-turorials");
+//reply to address
+    define('REPLY_TO',"jagathdammika17595@gmail.com");
+
+    define('REPLY_TO_NAME',"George");
