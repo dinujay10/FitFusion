853be103506b3a550b6a85a0a4bbb67e5d5b5c9d
jagath <jagathdammika17595@gmail.com>
2024-04-12 20:12:42 +0530

    workout plns controller

---
+++ app/models/Workoutplans.php
@@ -0,0 +1,41 @@
+<?php
+
+// user class
+class Workoutplans {
+    use Model;
+
+    protected $table = 'workoutplans';
+    
+    protected $allowedColumns = [
+        'id',
+        'iemail',
+        'maingoal',
+        'workouttype',
+        'traininglevel',
+        'programduration',
+        'daysperweek',
+        'targetgender',
+        'exercise',
+        'machine',
+        'time',
+        'rest'
+        
+    ];
+
+    public function unique($data) {
+        $this->errors = [];
+        
+        if(!empty($unique)) {
+        
+        }
+
+        if(empty($this->errors)) {
+            return true;
+        }
+        return false;
+    }
+
+  
+}
+
+// make models for each table in DB, and add the editable columns of each
