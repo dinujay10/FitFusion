6657b3199a06d5b801bc55a82fc5cef7cda8e87e
jagath <jagathdammika17595@gmail.com>
2024-04-20 09:13:22 +0530

    task list

--- app/views/taskList.view.php
+++ app/views/taskList.view.php
@@ -5,8 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/sidebarnew.css">
-    <link rel="stylesheet" href="<?=ROOT?>/assets/css/ManagerDashBoardStyle.css"> 
+
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/Sidebar.css"> 
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/MemberStyles.css"> 
+
+    <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/sidebarnew.css"> -->
+    <!-- <link rel="stylesheet" href="<?=ROOT?>/assets/css/ManagerDashBoardStyle.css">  -->
     <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/sidebarfinal.css"> -->
     <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/scheduleAppointRe.css"> -->
     <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/tasklist.css"> -->
@@ -14,104 +18,41 @@
 </head>
 
 <body>
-    <!-- <div class="side-bar">
-        <nav class="sidebar sidebar-offcanvas" id="sidebar">
-            <div class="nav-container">
-                <div class="top-container">
-                    <div class="center-image">
-                        <img src="<?=ROOT?>/assets/images/LogoFinal.png" alt="logo">
-                    </div>
-                    <div style="width: 100%">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="memberdash">
-                                    <span class="menu-title">Dashboard</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span class="menu-title">My Profile</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="current-nav-link" href="taskList">
-                                    <span class="menu-title">Task List</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="attendance">
-                                    <span class="menu-title">Attendance</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="memberpackages">
-                                    <span class="menu-title">Packages</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="scheduleInstrAppointReq">
-                                <span class="menu-title">Meetings</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="makeComplaint">
-                                    <span class="menu-title">Complaints</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="makereview">
-                                    <span class="menu-title">Reviews</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    <span class="menu-title">Payments</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="bottom-container">
-                    <button class="logout-button">
-                        <a  href="#">
-                            <span>Logout</span>
-                        </a>
-                    </button>
-                </div>
-            </div>
-        </nav>
-    </div> -->
 
     <!-- //////////////////////////// -->
-    <div class="side-menu">
-        <div class="brand-name">
-            <h1>FIT FUSION</h1>
-        </div>
-        <ul>
-        <a href="memberdash"><li>&nbsp; <h5>Dashboard</h5> </li></a>
-            <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/profile.jpeg" alt="">&nbsp;<h6>My Profile</h6> </li></a>
-            <a href="taskList"><li><img src="<?=ROOT?>/assets/images/dashboards/schedule.jpeg" alt="">&nbsp;<h6>Task List</h6> </li></a>
-            <a href="addgym"><li><img src="<?=ROOT?>/assets/images/dashboards/workout.jpeg" alt="">&nbsp;<h6>Attendance</h6> </li></a>
+    <div class="container-left">
+        <div class="side-menu">
+            <div class="brand-name">
+                <h1>FIT FUSION</h1>
+            </div>
+            <ul>
+                <a href="memberdash"><li>&nbsp; <h5>Dashboard</h5> </li></a>
+                <a href="memberprofile"><li><img src="<?=ROOT?>/assets/images/dashboards/profile.jpeg" alt="">&nbsp;<h6>My Profile</h6> </li></a>
+                <a href="taskList"><li><img src="<?=ROOT?>/assets/images/dashboards/schedule.jpeg" alt="">&nbsp;<h6>Task List</h6> </li></a>
+                <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/workout.jpeg" alt="">&nbsp;<h6>Attendance</h6> </li></a>
 
-            <a href="manageresources"><li><img src="<?=ROOT?>/assets/images/dashboards/failure.jpeg" alt="">&nbsp;<h6>Packages</h6> </li></a>
-            <a href="scheduleInstrAppointReq"><li><img src="<?=ROOT?>/assets/images/dashboards/setting.png" alt="">&nbsp;<h6>Meetings</h6> </li></a>
-            <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/task list.jpeg" alt="">&nbsp;<h6>Complaints</h6> </li></a>
-            <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/meeting.jpeg" alt="">&nbsp;<h6>Reviews</h6> </li></a>
-            <a href="home"><li><img src="<?=ROOT?>/assets/images/dashboards/help.jpeg" alt="">&nbsp; <h6>Payments</h6></li></a>
-            <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/setting.png" alt="">&nbsp;<h6>Logout</h6> </li></a>
+                <a href="scheduleInstrAppointReq"><li><img src="<?=ROOT?>/assets/images/dashboards/failure.jpeg" alt="">&nbsp;<h6>Meetings</h6> </li></a>
+                <a href="makeComplaint"><li><img src="<?=ROOT?>/assets/images/dashboards/task list.jpeg" alt="">&nbsp;<h6>Complaints</h6> </li></a>
+                <a href="makereviewgym"><li><img src="<?=ROOT?>/assets/images/dashboards/meeting.jpeg" alt="">&nbsp;<h6>Reviews</h6> </li></a>
+                <!-- <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/help.jpeg" alt="">&nbsp; <h6>Payments</h6></li></a> -->
+                <a href="scheduleinterface"><li><img src="<?=ROOT?>/assets/images/dashboards/meeting.jpeg" alt="">&nbsp;<h6>My schedule</h6></li></a>
+                <a href="memberviewmealplan"><li><img src="<?=ROOT?>/assets/images/dashboards/setting.png" alt="">&nbsp;<h6>Meal Plan</h6> </li></a>
+ 
+                <a href="#"><li><img src="<?=ROOT?>/assets/images/dashboards/setting.png" alt="">&nbsp;<h6>Logout</h6> </li></a>
 
-        </ul>
+            </ul>
         </div>
+    </div>
         <!-- //////////////////////////// -->
 
 
  <!-- ADDED THE CONTAINER CLASS -->
-    <div class="container">
+    <div class="container-right">
 
-    <div class="form" style="margin-left:30%">
-        <div class="form-item">
-                <p>Task List</p>
-        </div>
+        <div class="form">
+            <div class="form-content">
+                    <p>Task List</p>
+            </div>
 
         <!-- <div class="form-item">
             <input type="checkbox" id="machine1" name="machine1" value="machine1">
@@ -130,44 +71,44 @@
             <p>Leg Curl</p>
         </div> -->
 
-        <div class="container">
-            <div id="newtask">
-                <input type="text" placeholder="Add Tasks">
-                <button id="push">Add</button>
-            </div>
-            <div id="tasks"></div>
+        <!-- <div class="container"> -->
+                <div id="newtask">
+                    <input type="text" placeholder="Add Tasks">
+                    <button id="push">Add</button>
+                </div>
+                <div id="tasks"></div>
 
-            <script>
-                document.querySelector('#push').onclick = function(){
-                    if(document.querySelector('#newtask input').value.length == 0){
-                        alert("Kindly Enter Task Name!!!!")
-                    }
+                    <script>
+                        document.querySelector('#push').onclick = function(){
+                            if(document.querySelector('#newtask input').value.length == 0){
+                                alert("Kindly Enter Task Name!!!!")
+                            }
 
-                    else{
-                        document.querySelector('#tasks').innerHTML += `
-                            <div class="task">
-                                <span id="taskname">
-                                    ${document.querySelector('#newtask input').value}
-                                </span>
-                                <button class="delete">
-                                    <i class="far fa-trash-alt"></i>
-                                </button>
-                            </div>
-                        `;
+                            else{
+                                document.querySelector('#tasks').innerHTML += `
+                                    <div class="task">
+                                        <span id="taskname">
+                                            ${document.querySelector('#newtask input').value}
+                                        </span>
+                                        <button class="delete">
+                                            <i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </div>
+                                `;
 
-                        var current_tasks = document.querySelectorAll(".delete");
-                        for(var i=0; i<current_tasks.length; i++){
-                            current_tasks[i].onclick = function(){
-                                this.parentNode.remove();
+                                var current_tasks = document.querySelectorAll(".delete");
+                                for(var i=0; i<current_tasks.length; i++){
+                                    current_tasks[i].onclick = function(){
+                                        this.parentNode.remove();
+                                    }
+                                }
                             }
                         }
-                    }
-                }
-            </script>
+                    </script>
 
-        </div>
+                </div>
 
-    </div>
+        <!-- </div> -->
     </div>
 </body>
 
