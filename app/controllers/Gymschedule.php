bb38b1eaa0cd73901cdcec2c424720a1274b6519
Dinu Jay <damsinijayatissa@gmail.com>
2024-04-25 20:13:37 +0530

    updated the gym schedule controller

--- app/controllers/Gymschedule.php
+++ app/controllers/Gymschedule.php
@@ -7,11 +7,43 @@ class Gymschedule {
             $data = [];     
 
             $user = new User;
-            $arr2['email'] = $_SESSION['email'];
-            $memberdeets = $user->first($arr2);
+            $arr1['email'] = $_SESSION['email'];
+            $memberdeets = $user->first($arr1);
             $data['firstname'] = $memberdeets->name;
             $data['lastname'] = $memberdeets->lastname;
+
+            // print_r($memberdeets);
             
+            // SHOW THE EQUIPMENT OF THE MEMBER
+
+            // step 1 -> get the workout ID
+            $regmember = new Registeredmembers;
+            $arr2['memberemail'] = $_SESSION['email'];
+            // print_r($arr2['memberemail']);
+            $regmemberdeets = $regmember->first($arr2);
+
+            // print_r($regmemberdeets);
+            $data['workoutid'] = $regmemberdeets->workoutid;
+            // print_r($data['workoutid']);
+
+            // step 2 -> get the workout plan rows
+            $workoutplantable = new Workoutplans;
+            $arr3['id'] = $data['workoutid'];
+            $allworkoutplandeets = $workoutplantable->where($arr3);
+            // print_r($allworkoutplandeets);
+
+            // step 3 -> get all the machines into an array
+            $allmachines = [];
+            for ($i=0; $i < count($allworkoutplandeets); $i++) { 
+                array_push($allmachines, $allworkoutplandeets[$i]->machine);
+            }
+            // print_r($allmachines);
+
+            // step 4 -> add the machines array to the $data
+            $data['allmachines'] = $allmachines;
+            print_r($data);
+
+
             if ($_SERVER['REQUEST_METHOD']=='POST') {
 
             }
