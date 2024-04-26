f61ebf1ea8c5d505900f6a86e4c78930157c98cd
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:13:09 +0530

    open hours

--- app/models/Openhours.php
+++ app/models/Openhours.php
@@ -33,6 +33,28 @@ class Openhours {
         return false;
     }
 
+    public function validate($data){
+        $this->errors = [];
+
+        //$temp3=["openhourswf","openhourswt","openhourssaf","openhourssat","openhourssuf","openhourssut"];
+
+        if(strtotime($data["openhourswf"])>strtotime($data["openhourswt"])){
+            $this->errors['openhourswf'] = "Weekdays from time should be smaller than to time";
+        }
+        if(strtotime($data["openhourssaf"])>strtotime($data["openhourssat"])){
+            $this->errors['openhourssaf'] = "Saturday  from time should be smaller than to time";
+        }
+        if(strtotime($data["openhourssuf"])>strtotime($data["openhourssut"])){
+            $this->errors['openhourssuf'] = "Sunday from time should be smaller than to time";
+        }
+        
+
+        if(empty($this->errors)) {
+            return true;
+        }
+        return false;
+    }
+
   
 }
 
