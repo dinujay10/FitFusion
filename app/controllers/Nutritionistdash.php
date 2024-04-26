2c7c1b70c56d29d04f9dcbfee0a710045bb61a48
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:10:50 +0530

    nutri dash

--- app/controllers/Nutritionistdash.php
+++ app/controllers/Nutritionistdash.php
@@ -10,28 +10,28 @@ class Nutritionistdash {
             redirect('login');
          }
 
-        $gym = new Gym;
-        $address=new Address;
-        $gymimages=new Gymimages;
+        // $gym = new Gym;
+        // $address=new Address;
+        // $gymimages=new Gymimages;
 
-        $gymdata = $gym->findAll();
-        for($x=0;$x<count($gymdata);$x++){
-            $data[$x]['gymName']=$gymdata[$x]->gymName;
-            $data[$x]['id']=$gymdata[$x]->id;
+        // $gymdata = $gym->findAll();
+        // for($x=0;$x<count($gymdata);$x++){
+        //     $data[$x]['gymName']=$gymdata[$x]->gymName;
+        //     $data[$x]['id']=$gymdata[$x]->id;
 
-            $arr3['gymName']= $data[$x]['gymName'];
-            $locationdata=$address->first($arr3);
+        //     $arr3['gymName']= $data[$x]['gymName'];
+        //     $locationdata=$address->first($arr3);
     
-            $data[$x]['location1']=$locationdata->location1;
-            $data[$x]['location2']=$locationdata->location2;
-            $data[$x]['location3']=$locationdata->location3;
+        //     $data[$x]['location1']=$locationdata->location1;
+        //     $data[$x]['location2']=$locationdata->location2;
+        //     $data[$x]['location3']=$locationdata->location3;
 
-            //get images
-            $arr4['manageremail']=$gymdata[$x]->manageremail;
-            $gymimagesdata=$gymimages->first($arr4);
-            $data[$x]['gymimages']=$gymimagesdata->image_url;
-            //print_r($gymimagesdata);
-        }
+        //     //get images
+        //     $arr4['manageremail']=$gymdata[$x]->manageremail;
+        //     $gymimagesdata=$gymimages->first($arr4);
+        //     $data[$x]['gymimages']=$gymimagesdata->image_url;
+        //     //print_r($gymimagesdata);
+        // }
 
         $this->view('nutritionistdash',$data);
     }
