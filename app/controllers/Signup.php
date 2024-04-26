e9630bb5d618dd9e555654097dc66dfa9ecafc30
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:11:32 +0530

    sign up

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -6,35 +6,53 @@ class Signup {
 
     public function index() {
         if(isset($_SESSION['email'])){
-            redirect('gymmanagerdash');
-            die();
+            if($_SESSION['usertype']=="member") {
+                redirect('memberdash');
+                die();
+            } else if($_SESSION['usertype']=="gyminstructor") {
+                redirect('gyminstructordash');
+                die();
+            } else if($_SESSION['usertype']=="nutritionist") {
+                redirect('nutritionistdash');
+                die();
+            } else if($_SESSION['usertype']=="gymmanager") {
+                redirect('gymmanagerdash');
+                die();
+            } else if($_SESSION['usertype']=="gymowner") {
+                redirect('gymownerdash');
+                die();
+            } else if($_SESSION['usertype']=="admin") {
+                redirect('admindash');
+                die();
+            }
         }
         
         $data = [];
 
         if ($_SERVER['REQUEST_METHOD']=='POST') {
-            if($_POST['usertype']=='member') {
-                $user = new Member;
-            } else if($_POST['usertype']=='gyminstructor') {
-                $user = new Gyminstructor; 
-            } else if($_POST['usertype']=='nutritionist') {
-                $user = new Nutritionist;
-            }
+            // if($_POST['usertype']=='member') {
+            //     $user = new Member;
+            // } else if($_POST['usertype']=='gyminstructor') {
+            //     $user = new Gyminstructor; 
+            // } else if($_POST['usertype']=='nutritionist') {
+            //     $user = new Nutritionist;
+            // }
             // } else if($_POST['usertype']=='gymmanager') {
             //     $user = new Gymmanager;
             // } else if($_POST['usertype']=='gymowner') {
             //     $user = new Gymowner;
             // }
             
-            $usertable = new User;
+            $user = new User;
 
-            if($user->validate($_POST) && $usertable->unique($_POST)){
+            if($user->validate($_POST) && $user->unique($_POST)){
                 if($_POST['password']==$_POST['passwordConfirm']) {
                     $enc_password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+                    $_POST['usertype']="member";
                     $_POST['password']=$enc_password;
                     $_POST['passwordConfirm']=$enc_password;
                     $user->insert($_POST);
-                    $usertable->insert($_POST);
+                   // $usertable->insert($_POST);
                     redirect('login');
                 }
             }
