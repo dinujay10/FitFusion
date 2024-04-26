a87122c51220774d3d0057b1f4e3aa5fab4ccf3c
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:10:34 +0530

    mark attendence

--- app/controllers/Markattendance.php
+++ app/controllers/Markattendance.php
@@ -18,7 +18,13 @@ class Markattendance{
                 if(!empty($mem)){
                     $arr3['manageremail']=$_SESSION['email'];
                     $arr3['memberemail']=$mem[0]->memberemail;
+                    date_default_timezone_set('Asia/Colombo');
+                    $arr3['datee']=date('Y-m-d');
+                    $arr3['time']=date('H:i:s');
+                    $arr3['weekdy']=date('l');
                     $att->insert($arr3);
+                    redirect("markattendance");
+                    die();
                 }
                 else{
                     // error handling part
