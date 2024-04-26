2ab327a34d6f10557ffaf375dac088e3baa18f0f
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:10:42 +0530

    member dash

--- app/controllers/Memberdash.php
+++ app/controllers/Memberdash.php
@@ -4,9 +4,36 @@ class Memberdash {
     use Controller;
 
     public function index() {
-        if(!isset($_SESSION['email'])){
-            redirect('login');
-         }
+        
+        if(isset($_SESSION['email'])){
+            if($_SESSION['usertype']=="member") {
+               // redirect('memberdash');
+               
+            } else if($_SESSION['usertype']=="gyminstructor") {
+                redirect('gyminstructordash');
+                die();
+               
+            } else if($_SESSION['usertype']=="nutritionist") {
+                redirect('nutritionistdash');
+                die();
+               
+            } else if($_SESSION['usertype']=="gymmanager") {
+                redirect('gymmanagerdash');
+                die();
+               
+            } else if($_SESSION['usertype']=="gymowner") {
+                redirect('gymownerdash');
+                die();
+               
+            } else if($_SESSION['usertype']=="admin") {
+                redirect('admindash');
+                die();
+               
+            }
+        }
+        else{
+            redirect("login");
+        }
 
         $gym = new Gym;
         $address=new Address;
