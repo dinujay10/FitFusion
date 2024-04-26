b5c97bc1ee2ca20b2537d752d3113ca90a464f27
jagath <jagathdammika17595@gmail.com>
2024-04-26 12:14:54 +0530

    admin dash

--- app/views/admindash.view.php
+++ app/views/admindash.view.php
@@ -1,44 +1,242 @@
 <!DOCTYPE html>
 
+
 <html>
     <head>
         <title>FitFusion</title>
-        <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/style.css">
-
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
-
-        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+        <link rel="stylesheet" href="<?=ROOT?>/assets/css/ManagerDashBoardStyle.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
     </head>
     <body>
-        <header>
-            <a href="#" class = "logo">
+        <!-- <header> -->
+            <!-- <a href="#" class = "logo">
                 <img src="<?=ROOT?>/assets/images/LogoFinal.png" alt="logo">
-            </a>
-            <ul class="navmenu">
-                <li><a href ="home">Log Out</a></li>
-            </ul>
+            </a> -->
+            <!-- <ul class="navmenu"> -->
+                <!-- <li><a href ="home">Home</a></li>
+                <li><a href ="signup">Sign Up</a></li> -->
+                <!-- <li><a href ="home">Log Out</a></li>
+            </ul> -->
 
-            <div class="nav-icon">
-                <a href="#"><i class='bx bx-search-alt'></i></a>
+            <!-- <div class="nav-icon"> -->
+                <!-- <a href="#"><i class='bx bx-search-alt'></i></a>
                 <a href="searchgyms"><i class='bx bx-dumbbell' ></i></a>
-                <a href="searchnutritionists"><i class='bx bxs-bowl-rice'></i></a>
+                <a href="searchnutritionists"><i class='bx bxs-bowl-rice'></i></a> -->
 
                 <!-- <div class="bx bx-menu" id="menu-icon"></div> -->
-                <label for="check" class="checkbtn">
+                <!-- <label for="check" class="checkbtn">
                     <i class="fa fa-bars"></i>
                 </label>
             </div>
-        </header>
+        </header> -->
 
-        <div class="admin-container">
-            <div class="box"><a href="addgymmanager">Add Gym Manager</a></div>
-            <div class="box"><a href="addgymowner">Add Gym Owner</a></div>
-            <div class="box"><a href="#">Block Users</a></div>
-            <div class="box"><a href="#">Handle System Complaints</a></div>
+        <!-- <nav>
+            <ul>
+                <li>
+                    <a href="#" class="sidebar-logo">
+                    <img src="<?=ROOT?>/assets/images/Logo.png" alt="">
+                    <span class="nav-item">Member Dashboard</span>
+                </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="material-symbols-outlined">account_circle</span>
+                        <span class="nav-item">Profile</span>                    
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="material-symbols-outlined">schedule</span> 
+                        <span class="nav-item">Gym Schedule</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="scheduleinterface">
+                        <span class="material-symbols-outlined">schedule</span> 
+                        <span class="nav-item">My Schedule</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="material-symbols-outlined">monitoring</span> 
+                        <span class="nav-item">My Progress</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="material-symbols-outlined">task</span> 
+                        <span class="nav-item">Task List</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="material-symbols-outlined">groups</span> 
+                        <span class="nav-item">Meetings</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="complaintc">
+                        <span class="material-symbols-outlined">star</span> 
+                        <span class="nav-item">Make Complaints</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="logout" class="logout">
+                        <span class="material-symbols-outlined">logout</span> 
+                        <span class="nav-item">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </nav> -->
+
+
+        <!-- //////////////////////////// -->
+
+        <!-- //////////////////////////// -->
+
+        <!-- ADDED THE CONTAINER CLASS -->
+<div class="main-container">
+<div class="side-bar-container">
+            <div class="logo-tab">
+                FITFUSION
+            </div>
+            <ul class="side-bar-content">
+                
+                <!-- <li class="current-side-bar-tile">
+                    <div class="sb-tab-content">
+                        <span class="material-symbols-outlined">
+                            dashboard
+                        </span>
+                    </div>
+                    <div class="sb-tab-content">
+                        Dashboard
+                    </div>
+                </li> -->
+
+                <a class="side-bar-tile-link" href="admindash">
+                    <li class="side-bar-tile">
+                        <div class="sb-tab-content">
+                            <span class="material-symbols-outlined">
+                                home
+                            </span>
+                        </div>
+                        <div class="sb-tab-content">
+                            Dashboard
+                        </div>
+                    </li>
+                </a>
+                <a class="side-bar-tile-link" href="appliedmanagers">
+                    <li class="side-bar-tile">
+                        <div class="sb-tab-content">
+                            <span class="material-symbols-outlined">
+                                home
+                            </span>
+                        </div>
+                        <div class="sb-tab-content">
+                           Manager Applications
+                        </div>
+                    </li>
+                </a>
+                <a class="side-bar-tile-link" href="admincomplaints">
+                    <li class="side-bar-tile">
+                        <div class="sb-tab-content">
+                            <span class="material-symbols-outlined">
+                                home
+                            </span>
+                        </div>
+                        <div class="sb-tab-content">
+                           Complaints
+                        </div>
+                    </li>
+                </a>
+
+                <a class="side-bar-tile-link" href="">
+                    <li class="side-bar-tile">
+                        <div class="sb-tab-content">
+                            <span class="material-symbols-outlined">
+                                fitness_center
+                            </span>
+                        </div>
+                        <div class="sb-tab-content">
+                            #
+                        </div>
+                    </li>
+                </a>
+
+   
+
+                <a class="side-bar-tile-link" href="logout">
+                    <li class="side-bar-tile">
+                        <div class="sb-tab-content">
+                            <span class="material-symbols-outlined">
+                                logout
+                            </span>
+                        </div>
+                        <div class="sb-tab-content">Logout</div>
+                    </li>
+                </a>
+
+            </ul>
         </div>
+    <div class="container">
+    <div class="body-header">
+                <div class="pfp">
+                    <!-- LET THE INSTRUCTOR UPLOAD A PFP, OR KEEP A DEFAULT IMAGE -->
+                    <img src="#" alt="">
+                </div>
+                <div class="welcome-user">
+                    <!-- TODO - SHOW LOGGED IN INSTRUCTOR'S NAME -->
+                    Welcome, Admin
+                </div>
+</div>
+   
+               
+           
+                <!-- <?php
 
-         
+
+
+                    for($x=0;$x<count($data);$x++){
+                    echo '<a href="gympagetest?id='.$data[$x]['id'].'">
+                    <div class="gym-row">
+                    <img src="'.ROOT.'/assets/uploadgymimages/'.$data[$x]['gymimages'].'" alt="">
+                    
+                    <div class="gym-text">
+                        <h5>'.$data[$x]['location2'].', '.$data[$x]['location3'].'</h5>
+                    </div>
+                    
+                    <div class="rating">
+                        <i class="bx bx-star" ></i>
+                        <i class="bx bx-star" ></i>
+                        <i class="bx bx-star" ></i>
+                        <i class="bx bx-star" ></i>
+                        <i class="bx bx-star" ></i>
+
+                    </div>
+                    <div class="price">
+                        <h4>'.$data[$x]['gymName'].'</h4>
+                        <p>Rs. 3000 onwards</p>
+                    </div>
+                </div>   </a> '  ;
+                    }
+                ?> -->
+                
+
+               
+            </div>
+        </section>
+
+
+
+    </div>
+    <!-- CLOSED THE CONTAINER CLASS DIV -->
+
+        <!-- footer -->
+     
+<!-- 
+        <script src="<?=ROOT?>/assets/js/java.js"></script> -->
+</div>
     </body>
 </html>
