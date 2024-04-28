ead7505144fb2d63cd10d8fefa6002bb3223d827
Dinu Jay <damsinijayatissa@gmail.com>
2024-04-28 18:56:46 +0530

    updated member dashboard

--- app/views/Member/memberdash.view.php
+++ app/views/Member/memberdash.view.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/Member/main-template.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/Member/main-template.css">
 
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/css/Member/body-template.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/Member/body-template.css">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
+
 <body>
+    <?php
+    // echo '<pre>';
+    // var_dump($data);
+    // echo '</pre>';
+    ?>
     <div class="main-container">
         <div class="side-bar-container">
             <div class="logo-tab">
                 FITFUSION
             </div>
             <ul class="side-bar-content">
-                
+
                 <li class="current-side-bar-tile">
                     <div class="sb-tab-content">
                         <span class="material-symbols-outlined">
@@ -110,13 +117,10 @@
         </div>
         <div class="body-container">
             <div class="body-header">
-                <div class="pfp">
-                    <!-- LET THE MEMBER UPLOAD A PFP, OR KEEP A DEFAULT IMAGE -->
-                    <img src="#" alt="">
-                </div>
+
                 <div class="welcome-user">
                     <!-- TODO - SHOW LOGGED IN MEMBER'S NAME -->
-                    Welcome, 
+                    Welcome,
                     <?php
                     echo $data['firstname'] . " " . $data['lastname'];
                     ?>
@@ -125,19 +129,7 @@
             <div class="body-content">
                 <div class="content-tile-column">
                     <div class="tall-tile">
-                        <div class="pfp-tile">
-                            <div class="pfp-big"></div>
-                            <div class="db-button-tile">
-                                <!-- <button class="db-button"> -->
-                                    <a class="db-edit-button-link" href="editmemberprofile">
-                                        <span class="material-symbols-outlined">
-                                            edit
-                                        </span>    
-                                        Edit
-                                    </a>
-                                <!-- </button> -->
-                            </div>
-                        </div>
+
                         <div class="profile-info">
                             <div class="profile-info-key">First Name:</div>
                             <div class="profile-info-value">
@@ -168,8 +160,7 @@
                                 <?php
                                 if ($data['height']) {
                                     echo $data['height'] . 'cm';
-                                }
-                                else {
+                                } else {
                                     echo 'Add Height';
                                 }
                                 ?>
@@ -181,9 +172,8 @@
                                 <?php
                                 if ($data['weight']) {
                                     echo $data['weight'] . 'kg';
-                                }
-                                else {
-                                     echo 'Add Weight';
+                                } else {
+                                    echo 'Add Weight';
                                 }
                                 ?>
                             </div>
@@ -196,45 +186,75 @@
                                 ?>
                             </div>
                         </div>
+                        <div class="pfp-tile">
+                            <div class="db-button-tile">
+                                <!-- <button class="db-button"> -->
+                                <a class="db-edit-button-link" href="editmemberprofile">
+                                    <span class="material-symbols-outlined">
+                                        edit
+                                    </span>
+                                    Edit
+                                </a>
+                                <!-- </button> -->
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="content-tile-column">
-                    <div class="content-tile">
+                    <div class="content-tile" style="height: 70%;">
                         <div class="content-info">
                             <div class="content-info-key">Registered Gym:</div>
-                            <div class="content-info-value">Backend data</div>
+                            <div class="content-info-value"><?php echo $data['gymname']; ?></div>
+                        </div>
+                        <div class="content-info">
+                            <div class="content-info-key">Gym Email:</div>
+                            <div class="content-info-value"><?php echo $data['gymemail']; ?></div>
                         </div>
                         <div class="content-info">
                             <div class="content-info-key">Gym Location:</div>
-                            <div class="content-info-value">Backend data</div>
+                            <div class="content-info-value"><?php echo $data['location1'] . ', ' . $data['location2'] . ', ' . $data['location3']; ?></div>
                         </div>
                         <button class="db-link-button">Change Gym</button>
                     </div>
-                    
+
                 </div>
                 <div class="content-tile-column">
-                    <div class="content-tile">
+                    <div class="content-tile" style="height: 70%;">
                         <div class="content-info">
                             <div class="content-info-key">Selected Package:</div>
-                                <div class="content-info-value">
-                                    Backend Data
-
-                                    <!-- add a colum named STATUS and only get the current package that the member has. NOT the cancelled ones -->
-                                    <?php 
-                                    // echo $data['packagetype'] . " Package";
-                                    ?>
-                                </div>
+                            <div class="content-info-value">
+                                <!-- add a colum named STATUS and only get the current package that the member has. NOT the cancelled ones -->
+                                <?php
+                                echo $data['packagetype'];
+                                ?>
                             </div>
+                        </div>
+                        <div class="content-info">
+                            <div class="content-info-key">Package Group:</div>
+                            <div class="content-info-value">
+                                <!-- add a colum named STATUS and only get the current package that the member has. NOT the cancelled ones -->
+                                <?php
+                                if ($data['packagetype'] == 'instructor') {
+                                    echo 'Instructor';
+                                }
+                                else {
+                                    echo 'Instructor & Nutritionist';
+                                }
+                                
+                                ?>
+                            </div>
+                        </div>
                         <div class="content-info">
                             <div class="content-info-key">Package Expiry Date:</div>
                             <div class="content-info-value">Backend data</div>
                         </div>
                         <button class="db-link-button">Change Package</button>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
